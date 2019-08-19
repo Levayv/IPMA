64e4649d99ab09d8266f3561dae6530506d0dc05
Lev <levonayvazyan43@gmail.com>
2019-08-19 11:27:19 +0400

    reducer optimised, using object.assign correctly

--- client/src/redux/reducer/root.js
+++ client/src/redux/reducer/root.js
@@ -2,25 +2,31 @@
 // todo define initialState and assign it as default value to root function
 // todo wrong action logicly read_all vs read
 import {CRUD_READ_ALL} from "../action-types";
+
 const initState = {
-    something: "" ,
-    list: [] ,
+    something: "",
+    list: [],
     page: null
 };
-function root(state = initState, action){
+
+function root(state = initState, action) {
     console.log("Reducer[Root]: Action type is " + action.type);
-    console.debug(" Reducer[Root]: Current state is ...");
+    console.debug("Reducer[Root]: Current state is ...");
     console.debug(state);
-    console.debug(" Reducer[Root]: Action payload is ...");
+    console.debug("Reducer[Root]: Action payload is ...");
     console.debug(action.payload);
-    
+
     if (action.type === CRUD_READ_ALL) {
-        let newState = Object.assign({}, state);
-        newState.list.push(action.payload.data)
-        //todo refactor ?
-        console.debug(" Reducer[Root]: New state will be ...");
-        console.debug(newState);
-        return newState;
+        // let newState = Object.assign({}, state);
+        // newState.list.push(action.payload.data)
+        // console.debug(" Reducer[Root]: New state will be ...");
+        // console.debug(newState);
+        // return newState;
+        let data = action.payload.data;
+        return Object.assign({}, state, {
+                list: state.list.concat(action.payload.data)
+            }
+        )
     }
     // switch (action.type) {
     //     case ADD_ARTICLE:
@@ -29,4 +35,5 @@ function root(state = initState, action){
     // }
     return state;
 }
+
 export default root;
